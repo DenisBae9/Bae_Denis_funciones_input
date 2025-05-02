71ccc03d18a2a4091d56ce0bd2b20f1aef83592e
DenisBae9 <denisbae025@gmail.com>
2025-05-02 01:44:19 -0300

    Update Validate.py

--- Validate.py
+++ Validate.py
@@ -1,1 +1,14 @@
-
+def validate_number(numero:int, max:int, min:int) -> bool:
+    if numero < min or numero > max:
+        validado = False
+    else:
+        validado = True
+    return validado
+        
+def validate_length(cadena:str, longitud:int) -> bool:
+    longitud_cadena = len(cadena)
+    if cadena == longitud:
+        validado = False
+    else:
+        validado = True
+    return validado
